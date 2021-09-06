2404a78c09b228a9a4e9895ca90d481b1de1fc24
Daniel Seldura <daniel.seldura@gmail.com>
2021-09-06 11:32:06 +0800

    Sample files

---
+++ 2_JS/LoopsTriangle.js
@@ -0,0 +1,7 @@
+var height = 6;
+var string = '';
+for (var i = 0; i < height; i++) {
+    string += `${i}`;
+    // string = string + `${i}`;
+}
+console.log(string);
---
+++ 2_JS/VariableType.js
@@ -0,0 +1,13 @@
+var number = 1;
+var a = [1, 2, 3, 4, 5, 6];
+var b = { one: 1, "two": "two", three: [3] };
+var c;
+var d = null;
+console.log(typeof d);
+console.log(`The variable ${b['one']} is of type ${typeof b.one}`);
+console.log(b);
+b.four = "HI ALL!";
+console.log(b);
+b["five"] = ["HAHAHAHAH", "MEOW"];
+console.log(b);
+console.log(b.six);
