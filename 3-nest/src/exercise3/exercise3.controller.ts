032829e48accc83a525b7064de1ef72dd4efbaa3
Daniel Seldura <daniel.seldura@gmail.com>
2021-09-08 19:22:16 +0800

    3-nest Assignment

--- 3-nest/src/exercise3/exercise3.controller.ts
+++ 3-nest/src/exercise3/exercise3.controller.ts
@@ -5,24 +5,9 @@ import { Exercise3Service } from './exercise3.service';
 export class Exercise3Controller {
   constructor(private readonly e3: Exercise3Service) {}
 
-  @Get('/helloWorld/:name')
-  getHello(@Param('name') name:string): string {
-    return this.e3.helloWorld(name);
-  }
-
   @Get('/loopsTriangle/:height')
   loopsTriangle(@Param('height') height: string) {
-    var parsedHeight = parseInt(height);
+    var parsedHeight:number = parseInt(height);
     return this.e3.loopsTriangle(parsedHeight);
   }
-
-  @Get('/loopsTriangle/:num')
-  primeNumber(num:number){
-    
-  }
-  //the anatomy of a typescript function
-  //   functionName(someparameterhere:theType):thereturntype {
-  //       thefunctioncode
-  //       return (must match return type);
-  //   }
 }
