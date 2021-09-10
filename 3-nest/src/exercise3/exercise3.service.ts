f403851a1cd63d263a5e2a234b1ead88af35d486
Daniel Seldura <daniel.seldura@gmail.com>
2021-09-10 13:18:15 +0800

    Added html helper

--- 3-nest/src/exercise3/exercise3.service.ts
+++ 3-nest/src/exercise3/exercise3.service.ts
@@ -1,17 +1,24 @@
+import { HTML } from './html.helper';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class Exercise3Service {
+   
+
+
   loopsTriangle(height: number) {
+    var html: HTML = new HTML(); 
     for (var i = 1; i <= height; i++) {
-      var string = '';
+      
+      var string = ''; 
       var j = i;
       while (j) {
         string += '*';
         j--;
       }
+      html.add(html.div([string]));
       console.log(string);
     }
-    return;
+    return html.renderScreenHTML();
   }
 }
