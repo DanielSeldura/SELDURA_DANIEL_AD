d547ec4407dadfa9f8febf5d3ba8f48271121038
Daniel Seldura <daniel.seldura@gmail.com>
2021-09-20 14:05:06 +0800

    Update

--- 3-nest/src/user/user.controller.ts
+++ 3-nest/src/user/user.controller.ts
@@ -1,14 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService:UserService){}
+  constructor(private readonly userService: UserService) {}
+  @Post('/register')
+  register(@Body() body: any) {
+    return this.userService.register(body);
+  }
 
-    @Get("/all")
-    getAll(){
-        return this.userService.getAll();
-    }
+  @Post('/login')
+  login(@Body('email') email: string, @Body('password') password: string) {
+    return this.userService.login(email, password);
+  }
+
+  @Get('/all')
+  getAllUser() {
+    return this.userService.getAll();
+  }
+  @Get('/search/:term')
+  searchUser(@Param('term') term: string) {
+    return this.userService.searchUser(term);
+  }
+
+  @Get('/:id')
+  getUserID(@Param('id') id: string) {
+    return this.userService.getOne(parseInt(id));
+  }
+
+  @Put('/:id')
+  replaceValuePut(@Param('id') id: string, @Body() body: any) {
+    return this.userService.replaceValuePut(parseInt(id), body);
+  }
+
+  @Patch('/:id')
+  replaceValuePatch(@Param('id') id: string, @Body() body: any) {
+    return this.userService.replaceValuePatch(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(parseInt(id));
+  }
+
+
 
 }
--- 3-nest/src/user/user.service.ts
+++ 3-nest/src/user/user.service.ts
@@ -3,26 +3,168 @@ import { User } from './user.model';
 
 @Injectable()
 export class UserService {
+  private users: Map<number, User> = new Map<number, User>();
+  email: string;
+  password: string;
 
-    private users: Map<number,User> = new Map<number,User>();
+  constructor() {
+    this.populate();
+  }
 
-    constructor(){
-        this.populate();
+  populate() {
+    try {
+      this.users.set(
+        1,
+        new User(
+          1,
+          'Wilfredo Quitara',
+          18,
+          'Wilfredo.Quitara@gmail.com',
+          '123456',
+        ),
+      );
+      this.users.set(
+        2,
+        new User(2, 'Kate Sasan', 16, 'Kate.Sasan@gmail.com', '1234567'),
+      );
+      this.users.set(
+        3,
+        new User(3, 'Maegan Chu', 17, 'Maegan.Chu@gmail.com', '1234568'),
+      );
+      this.users.set(
+        4,
+        new User(
+          4,
+          'Shannen Loyola',
+          18,
+          'Shannen.Loyola@gmail.com',
+          '1234569',
+        ),
+      );
+      this.logAllUsers();
+      console.log('Finished population');
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    getAll(){
-       var populatedData = [];
-       for(const user of this.users.values()){
-           populatedData.push(user.toJson());
-       }
-       return populatedData;
+  register(user: any): { success: boolean; message: string } {
+    try {
+      var validBody: { valid: boolean; error: string } = User.validBody(user);
+      if (validBody.valid) {
+        var newUser: User = new User(
+          user.id,
+          user.name,
+          user.age,
+          user.email,
+          user.password,
+        );
+        this.users.set(user.id, newUser);
+        this.logAllUsers();
+        return {
+          success: true,
+          message: 'Thank you for registering! Account has been added.',
+        };
+      } else {
+        return { success: false, message: validBody.error };
+      }
+    } catch (error) {
+      return { success: false, message: 'Error adding account' };
     }
+  }
 
-    populate(){
-        this.users.set(1,new User(1,"James",18,"james@email.com","123456"));
-        this.users.set(2,new User(2,"John",18,"john@email.com","143441"));
-        this.users.set(3,new User(3,"Luke",18,"luke@email.com","654321"));
-        this.users.set(4,new User(4,"Judas",13,"judas@email.com","696969"));
+  logAllUsers() {
+    console.log(this.getAll());
+  }
+
+  getAll() {
+    var populateData = [];
+    for (const user of this.users.values()) {
+      populateData.push(user.toJson());
     }
+    return populateData;
+  }
 
+  getOne(id: number) {
+    if (this.users.has(id)) {
+      return this.users.get(id).toJson();
+    } else
+      return {
+        success: false,
+        error: `User ${id} is not in database`,
+      };
+  }
+
+  replaceValuePut(id: number, user: any) {
+    if (this.users.has(id)) {
+      var validBodyPut: { valid: boolean; error: string } =
+        User.validBodyPut(user);
+      if (validBodyPut.valid) {
+        var newUser: User = new User(
+          id,
+          user.name,
+          user.age,
+          user.email,
+          user.password,
+        );
+        this.users.set(id, newUser);
+        this.logAllUsers();
+        return {
+          success: true,
+          data: newUser.toJson(),
+        };
+      } else return validBodyPut;
+    } else
+      return {
+        success: false,
+        error: `User ${id} is not in database`,
+      };
+  }
+
+  replaceValuePatch(id: number, user: any) {
+    if (this.users.has(id)) {
+      var userObj: User = this.users.get(id);
+      var result = userObj.patch(user);
+      this.logAllUsers();
+      return { result, data: userObj.toJson() };
+    } else
+      return {
+        valid: false,
+        error: `User ${id} is not in database`,
+      };
+  }
+
+  deleteUser(id: number) {
+    if (this.users.has(id)) {
+      return {
+        success: this.users.delete(id),
+        error: `User ${id} has been successfully removed`,
+      };
+    } else
+      return {
+        success: false,
+        error: `User ${id} is not in database`,
+      };
+  }
+
+  login(email: string, password: string) {
+    for (const user of this.users.values()) {
+      if (user.matches(email)) return user.login(password);
+    }
+    return { success: false, message: `${email} not found in database` };
+  }
+
+  searchUser(term: string) {
+      var results = [];
+    for (const user of this.users.values()) {
+      if (user.matches(term)) results.push(user.toJson());
+    }
+    if(results.length>0){
+        return results;
+    }
+    return {
+      success: false,
+      message: `${term} not matching any user entry in database`,
+    };
+  }
 }
