a362dd572cd8fdfc914f734e65d8a904ca19908d
Noel Daniel T. Seldura <daniel.seldura@gmail.com>
2021-11-29 11:07:30 +0800

    updated to 3001

--- 3-nest/src/main.ts
+++ 3-nest/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
